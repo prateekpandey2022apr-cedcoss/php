3e0f51c6ca64078040578ef283a161f11e4bc799
prateekpandey2022apr-cedcoss <prateekpandey2022apr@cedcoss.com>
2022-05-09 17:12:33 +0530

    Write a program to calculate factorial of a number using for loop in php.

--- php-basics-programs/index.php
+++ php-basics-programs/index.php
@@ -1,32 +1,19 @@
 <?php
 
-function weekday($num){
+function factorial($num){
 
-    switch ($num) {
-        case 1:            
-            return "Monday";
-        case 2:            
-            return "Tuesday";
-        case 3:            
-            return "Wednesday";
-        case 4:            
-            return "Thursday";
-        case 5:            
-            return "Friday";
-        case 6:            
-            return "Saturday";
-        case 7:            
-            return "Sunday";        
-        default:
-            return "Invalid Number";
+    $fact = 1;
+
+    for ($i = $num; $i > 1; $i--) { 
+        $fact *= $i;
     }
-    
+   
+    return $fact;
 }
 
 
-echo weekday(1) . "<br>";
-echo weekday(6) . "<br>";
-echo weekday(8). "<br>";
+echo factorial(5) . "<br>";
+echo factorial(0) . "<br>";
 
 
 ?>
