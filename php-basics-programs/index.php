0ddcfd5288c92cfba8f0cb33220156a4db61e4f9
prateekpandey2022apr-cedcoss <prateekpandey2022apr@cedcoss.com>
2022-05-09 17:01:51 +0530

    Write a program to check student grade based on the marks using if-else statement.~

--- php-basics-programs/index.php
+++ php-basics-programs/index.php
@@ -1,8 +1,23 @@
 <?php
 
-$var1 = "this is test 1";
-$var2 = "this is test 2";
+function find_grade($marks){
+    if($marks >= 60){
+        return "First Division";
+    }
+    elseif( $marks >= 45 && $marks <= 59 ){
+        return "Second Division";
+    }
+    elseif( $marks >= 33 && $marks <= 44 ){
+        return "Third Division";
+    }
+    else{
+        return "Fail";
+    }
+}
 
-echo $var1 . "<br>" . $var2;
+
+echo find_grade(95) . "<br>";
+echo find_grade(40) . "<br>";
+
 
 ?>
