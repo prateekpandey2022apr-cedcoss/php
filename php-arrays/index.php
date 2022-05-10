7d057a91abb88f3eceaa46860bc76769b9bdf9a5
prateekpandey2022apr-cedcoss <prateekpandey2022apr@cedcoss.com>
2022-05-10 10:38:22 +0530

    Write a PHP program to filter out some elements with certain key-names.

--- php-arrays/index.php
+++ php-arrays/index.php
@@ -1,53 +1,21 @@
 <?php
 
-    $recorded_temps = array(
-        78, 60, 62, 68, 71, 68, 73, 85, 66, 
-        64, 76, 63, 75, 76, 73, 68, 62, 73, 
-        72, 65, 74, 62, 62, 65, 64, 68, 73, 
-        75, 79, 73
+    $first_arr = array(
+        'c1' => 'Red', 
+        'c2' => 'Green', 
+        'c3' => 'White', 
+        'c4' => 'Black',
     );
 
-    $sum = 0;
+    $filter_by = array("c2", "c4");
 
-    foreach($recorded_temps as $tmp){
-        $sum += $tmp;
-    }
+    $new_array = array_filter($first_arr, function($key) use($filter_by){
+        // return $key !== "c2" && $key !== "c4";
+        return !in_array($key, $filter_by);
+    }, ARRAY_FILTER_USE_KEY);
 
-    $avg = $sum / count($recorded_temps);
-
-    echo "Average Temperature is:  " . round($avg, 2);
-
-    sort($recorded_temps);
-
-    $lowest_tmp = array();
-    $highest_tmp = array();
-    
-    // store lowest 5 temps
-    for ($i = 0; $i < 5; $i++) { 
-        $lowest_tmp[] = $recorded_temps[$i];
-    }
-        
-    echo "<br>";
-
-    echo "List of 5 lowest temperatures: ";
-
-    foreach($lowest_tmp as $tmp){
-        echo $tmp . " ";
-    }
-
-    echo "<br>";    
-
-    // store highest 5 temps
-    for ($i = count($recorded_temps) - 6; $i < count($recorded_temps) - 1 ; $i++) {         
-        $highest_tmp[] = $recorded_temps[$i];
-    }    
-
-    echo "List of 5 highest temperatures: ";
-
-    foreach($highest_tmp as $tmp){
-        echo $tmp . " ";
-    }
-
-    echo "<br>";
+    echo "<pre>";
+    var_dump($new_array);
+    echo "</pre>";
 
 ?>
