a04e6425ec70730221ed3ce20cfb066e03eed09c
prateekpandey2022apr-cedcoss <prateekpandey2022apr@cedcoss.com>
2022-05-11 17:50:24 +0530

    Write a program to upload image

--- php-forms/index.php
+++ php-forms/index.php
@@ -21,30 +21,64 @@
     <?php
 
     // echo "<pre>";
-    // var_dump($_POST);    
-    // echo "</pre>";    
+    // var_dump($_POST);
+    // echo "</pre>";
+
+    // echo "<hr>";
+
+    // echo "<pre>";
+    // var_dump($_FILES);
+    // echo "</pre>";
+
+    // echo "<pre>";    
+    // // var_dump($_FILES['image']['size'] / (1024 * 1024) );
+    // // var_dump($_FILES['image']['size'] / (1024 * 1024) > 2 );
+    // echo "</pre>";
 
     // $validation_errors = array();
 
-    if (isset($_POST["submit"])) {
-        process_form();
-    } else {
+    if (isset($_POST["submit"]) && 
+    $_FILES['image']['error'] == "UPLOAD_ERR_OK") 
+    {
+        if( $_FILES['image']['size'] / (1024 * 1024) > 2)
+        {
+            echo "Size > 2MB";
+        }
+        elseif( $_FILES['image']['type'] != "image/png" )
+        {
+            echo "File isn't PNG";            
+        }
+        else
+        {
+            // echo "uploading ...";
+
+            move_uploaded_file($_FILES['image']['tmp_name'], 
+            "./uploads/" . $_FILES['image']['name']);
+
+            echo "File Uploaded";            
+        }
+
+        display_form(array());
+        
+    } 
+    else 
+    {
         display_form(array());
     }
 
     function validate_field($field, $missing_fields)
     {
-        global $validation_errors;
+        // global $validation_errors;
 
         if(in_array($field, $missing_fields))
         {
             echo ' class="error"';
-        }        
+        }
     }
 
     function process_form()
-    {   
-        $required_fields = array( "input", "conversion");
+    {
+        $required_fields = array( "image",);
         $missing_fields = array();
 
 
@@ -56,7 +90,7 @@
         }
 
         if($missing_fields)
-        {
+        {            
             display_form($missing_fields);
         }
         else
@@ -65,22 +99,22 @@
             // echo "<pre>";
             // var_dump($_POST);
             // echo "</pre>";
-        
+
             // echo "called";
             // $num1_err = $num2_err = "";
 
-            $hrs = $_POST['input'];            
-            $conversion = $_POST['conversion'];
+            // $hrs = $_POST['input'];
+            // $conversion = $_POST['conversion'];
 
-            if($conversion == "min")
-            {
-                $_POST['result'] = $hrs * 60;
-            }
-            else
-            {
-                $_POST['result'] = $hrs * 60 * 60;
-            }
-        
+            // if($conversion == "min")
+            // {
+            //     $_POST['result'] = $hrs * 60;
+            // }
+            // else
+            // {
+            //     $_POST['result'] = $hrs * 60 * 60;
+            // }
+
             display_form(array());
             // echo "form submitted";
         }
@@ -88,47 +122,24 @@
 
     function display_form($missing_fields)
     {
+
+        // var_dump($missing_fields);
         // global $validation_errors;
-    ?>
-        <?php if ($missing_fields) { ?>
-            <p>There were some errors while trying to fill the form</p>
-        <?php } ?>
+    ?>        
 
 
-        <?php //var_dump($validation_errors); 
+        <?php //var_dump($validation_errors);
         ?>
 
-        <form action="" method="post">
+        <form action="" method="post" enctype="multipart/form-data">
 
-            <p>
-                <input type="text" name="input" id="input"
-                 value="<?php echo $_POST['input'] ?>"
-                 <?php echo validate_field("input", $missing_fields); ?>   >
-            </p>            
-
-            <p <?php echo validate_field("conversion", $missing_fields); ?> >
-                <label for="min">
-                    <input type="radio" name="conversion" value="min" id="min" 
-                    
-                    <?php if(isset($_POST['conversion']) && 
-                    $_POST['conversion'] == "min" ) echo "checked" ?>  >Hours to mins
-                </label>
-                <br>
-                <label for="sec">
-                    <input type="radio" name="conversion" value="sec" id="sec"
-                    <?php if(isset($_POST['conversion']) && 
-                    $_POST['conversion'] == "sec" ) echo "checked" ?>
-                    >Hours to secs
-                </label>                
+            <p>                
+                <input type="file" name="image" id="image">
             </p>
 
-            <?php if(isset($_POST['result'])) { ?>
-                <p><?php echo $_POST['result'] ?></p>
-            <?php } ?>            
-
             <p>
-                <input type="submit" name="submit" value="Convert" id="submit">
-            </p>       
+            <input type="submit" name="submit">
+            </p>
 
         </form>
 
